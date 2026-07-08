f732956d1497e346931121aa9e198b2724ae8183
Fazal <boyswild104@gmail.com>
2026-07-08 10:19:58 +0530

    Initial commit for restaurant website

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,10 @@ import preact from '@preact/preset-vite' // Fixed package name
 
 export default defineConfig({
   plugins: [preact()],
+  base: "/CelinaAsianBistro/",
   server: {
     port: 3000,
     open: true
   }
+
 })
